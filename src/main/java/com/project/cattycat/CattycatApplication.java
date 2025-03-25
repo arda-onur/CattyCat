57724694152fa7d9578e4c2b83cd328b18348223
Arda Onur <ardaonur15@gmail.com>
2025-03-25 16:17:30 +0300

    Files Created

---
+++ src/main/java/com/project/cattycat/CattycatApplication.java
@@ -0,0 +1,13 @@
+package com.project.cattycat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CattycatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CattycatApplication.class, args);
+	}
+
+}
