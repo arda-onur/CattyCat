474bab998cb8c30c55ca3dded3488be52416f4de
Arda Onur <ardaonur15@gmail.com>
2025-03-27 15:47:32 +0300

    Implemented Address VO

--- src/main/java/com/project/cattycat/domain/user/Address.java
+++ src/main/java/com/project/cattycat/domain/user/Address.java
@@ -1,4 +1,19 @@
 package com.project.cattycat.domain.user;
 
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
 public class Address{
+    private String country;
+    private String city;
+    private String zip;
+
+
+    public Address(String country, String city, String zip) {
+        this.country = country;
+        this.city = city;
+        this.zip = zip;
+    }
+
+
 }
