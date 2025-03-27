6ee1274922812377f99af447ce33e2c07c226e9d
Arda Onur <ardaonur15@gmail.com>
2025-03-27 16:17:00 +0300

    Implemented User class

--- src/main/java/com/project/cattycat/domain/user/User.java
+++ src/main/java/com/project/cattycat/domain/user/User.java
@@ -1,5 +1,20 @@
-package com.project.cattycat.domain;
+package com.project.cattycat.domain.user;
 
 public class User {
+    private final UserId id;
+    private final String email;
+    private final String password;
+    private final Address address;
 
+    public User(final UserId id, final String email, final String password) {
+        if (email.isBlank() || password.isBlank())
+            throw new IllegalArgumentException("Email or password cannot be blank.");
+        if (id == null)
+            throw new NullPointerException("id is null");
+
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.address = null;
+    }
 }
