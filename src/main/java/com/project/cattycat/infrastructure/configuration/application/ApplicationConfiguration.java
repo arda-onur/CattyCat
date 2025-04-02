7395f49c522ef72a563fb327dc721c26358b8f49
Arda Onur <ardaonur15@gmail.com>
2025-04-02 17:16:45 +0300

    Implemented Application and Encoder Configuration

--- src/main/java/com/project/cattycat/infrastructure/configuration/application/ApplicationConfiguration.java
+++ src/main/java/com/project/cattycat/infrastructure/configuration/application/ApplicationConfiguration.java
@@ -1,4 +1,40 @@
 package com.project.cattycat.infrastructure.configuration.application;
 
-public class ApplicationConfiguration {
+import com.project.cattycat.infrastructure.persistence.Entity.User.UserRepositoryImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfiguration implements WebMvcConfigurer {
+
+    private final UserRepositoryImpl userRepository;
+    private final EncoderConfiguration pwdEncoderConfiguration;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return  email -> (UserDetails) userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+    @Bean
+    public AuthenticationProvider authenticationcationProvider() {
+        DaoAuthenticationProvider daoAuthentiProvider = new DaoAuthenticationProvider();
+        daoAuthentiProvider.setUserDetailsService(userDetailsService());
+        daoAuthentiProvider.setPasswordEncoder(pwdEncoderConfiguration.bCryptPasswordEncoder());
+        return daoAuthentiProvider;
+    }
+
 }
