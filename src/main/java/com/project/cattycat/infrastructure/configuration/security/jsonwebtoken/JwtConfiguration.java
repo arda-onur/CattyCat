1bbddef51c1389cbe9509c6541557439b917f552
Arda Onur <ardaonur15@gmail.com>
2025-04-02 17:15:58 +0300

    Implemented JWT configuration and service

--- src/main/java/com/project/cattycat/infrastructure/configuration/security/jsonwebtoken/JwtConfiguration.java
+++ src/main/java/com/project/cattycat/infrastructure/configuration/security/jsonwebtoken/JwtConfiguration.java
@@ -1,18 +1,56 @@
 package com.project.cattycat.infrastructure.configuration.security.jsonwebtoken;
 
+import com.project.cattycat.domain.user.User;
+import com.project.cattycat.infrastructure.persistence.Entity.User.JpaUserRepository;
+import com.project.cattycat.infrastructure.persistence.Entity.User.UserEntity;
+import com.project.cattycat.infrastructure.persistence.Entity.User.UserRepositoryImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
+@Configuration
+@RequiredArgsConstructor
 public class JwtConfiguration extends OncePerRequestFilter {
 
 
+    private final JwtService jwtService;
+    private final JpaUserRepository userRepository;
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        String email = jwtService.extractEmail(token);
+
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            User user = userRepository.findByEmail(email).orElse(null);
+
+            if (user != null) {
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                        user, null, List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name())
+                ));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
+
 }
--- src/main/java/com/project/cattycat/infrastructure/configuration/security/jsonwebtoken/JwtService.java
+++ src/main/java/com/project/cattycat/infrastructure/configuration/security/jsonwebtoken/JwtService.java
@@ -1,4 +1,74 @@
 package com.project.cattycat.infrastructure.configuration.security.jsonwebtoken;
 
+import com.project.cattycat.infrastructure.persistence.Entity.User.JpaUserRepository;
+import com.project.cattycat.infrastructure.persistence.Entity.User.UserRepositoryImpl;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
 public class JwtService {
+
+
+    private static final String SECRET_KEY = "AaPrZ1fRfMVxWK/0KZaq6RheyHjoj5kO5yX6c2G5SM1O8QcTf3t6FjR6UD3VOQY6";
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+    public String generateToken(Map<String, Object> claims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60  *24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    public  boolean isTokenValid(String token,UserDetails userDetails) {
+        final String username = extractEmail(token);
+        return (username.equals(userDetails.getUsername())) &&  !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
+
