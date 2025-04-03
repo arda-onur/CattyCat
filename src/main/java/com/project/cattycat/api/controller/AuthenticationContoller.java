b152fcb1b0883962e964df88b9f4a1fc500707cd
Arda Onur <ardaonur15@gmail.com>
2025-04-03 14:45:57 +0300

    Implemented Register method

--- src/main/java/com/project/cattycat/api/controller/AuthenticationContoller.java
+++ src/main/java/com/project/cattycat/api/controller/AuthenticationContoller.java
@@ -1,5 +1,37 @@
 package com.project.cattycat.api.controller;
 
+import com.project.cattycat.application.authentication.AuthenticationService;
+import com.project.cattycat.application.dto.RegisterRequestDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class AuthenticationContoller {
 
+    private final AuthenticationService authenticationService;
+     @RequestMapping("/register")
+    public ResponseEntity<String> register(@RequestBody @Valid RegisterRequestDTO registerRequestDTO){
+        return ResponseEntity.ok().body(authenticationService.register(registerRequestDTO));
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
